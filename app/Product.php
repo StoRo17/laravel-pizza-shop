8b4bc5a8812c220f19831b7ec247f77f5649fa0f
storo17 <storo1017@gmail.com>
2016-11-12 11:38:40 +0000

    Start do admin panel

--- app/Product.php
+++ app/Product.php
@@ -8,6 +8,24 @@ use Illuminate\Database\Eloquent\Model;
  * App\Product
  *
  * @mixin \Eloquent
+ * @property integer $id
+ * @property string $category
+ * @property string $name
+ * @property integer $price
+ * @property integer $weight
+ * @property integer $diameter
+ * @property string $pathToImage
+ * @property string $composition
+ * @property string $description
+ * @method static \Illuminate\Database\Query\Builder|\App\Product whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Product whereCategory($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Product whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Product wherePrice($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Product whereWeight($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Product whereDiameter($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Product wherePathToImage($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Product whereComposition($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Product whereDescription($value)
  */
 class Product extends Model
 {
--- app/User.php
+++ app/User.php
@@ -11,6 +11,24 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $unreadNotifications
  * @mixin \Eloquent
+ * @property integer $id
+ * @property string $name
+ * @property string $phone_number
+ * @property string $email
+ * @property string $password
+ * @property boolean $is_admin
+ * @property string $remember_token
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User wherePhoneNumber($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereIsAdmin($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereUpdatedAt($value)
  */
 class User extends Authenticatable
 {
